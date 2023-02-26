575780d7ac327e70d4db61bb570e50a7cbcef981
EmD4blju <92537261+EmD4blju@users.noreply.github.com>
2023-02-26 13:41:50 +0100

    Update 26.02.2023
    Changes:
     - added night-mode switch

--- website/index.js
+++ website/index.js
@@ -1,18 +1,35 @@
 
-//------------------------------ "Return to top" button operation ------------------------------
+// "Return to top" button operation
 {
     let button = document.getElementById("toTopButton");
 
     window.onscroll = function(){scrollFunction()};
 
     function scrollFunction(){
-        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+        if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
             button.style.display = "block";
-        else
+        }
+        else{
             button.style.display = "none";
+        }
     }
 
     function getToTop(){
         document.documentElement.scrollTop = 0;
+    }
+}
+// Night-mode switch
+{   
+    function toggleModeSwitch(){
+        if(document.body.style.backgroundColor == "antiquewhite"){
+            document.body.style.backgroundColor = "#24252A";
+            document.body.style.color = "antiquewhite";
+            document.body.style.transition = "0.2s ease";
+            
+        }else{
+            document.body.style.backgroundColor = "antiquewhite";
+            document.body.style.color = "gray";
+            document.body.style.transition = "0.2s ease";
+        }
     }
 }
